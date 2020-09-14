ebdb0b6a7263ed6931f142d5fbe587dc1ebae5ca
David G <gomezd6@mcmaster.ca>
2020-09-14 11:25:29 -0400

    Updates

---
+++ precache-manifest.b343f58c6c99fe5cf3e38ec98d6f9fe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c118dd63a7a6be1288832a663285fc9",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "da25efa041ce7ff0a75e",
+    "url": "/app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ebf47723a1266f6d3823",
+    "url": "/app/static/js/2.f3b576d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/static/js/2.f3b576d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da25efa041ce7ff0a75e",
+    "url": "/app/static/js/main.b0d759ca.chunk.js"
+  },
+  {
+    "revision": "0ebf5a8caffaa2acdaa2",
+    "url": "/app/static/js/runtime-main.9aa1f20b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  }
+]);
